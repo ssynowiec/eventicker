740a6b111d42cb7f2ef923d73bed9fbbd7b7a3f6
Stanisław Synowiec <stasyn1410@gmail.com>
2024-08-21 14:09:56 +0200

    fix(auth): logout

--- src/app/api/auth/logout/route.tsx
+++
@@ -1,5 +0,0 @@
-import { logout } from '@/lib/auth/signOut';
-
-export const GET = async () => {
-	return logout();
-};
--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,16 +1,15 @@
-import { LogOut } from 'lucide-react';
 import { DropdownMenuItem } from '@/components/ui/dropdown-menu';
-import Link from 'next/link';
+import { logout } from '@/lib/auth/signOut';
+import { LogOut } from 'lucide-react';
 
 export const LogoutButton = () => {
 	return (
 		<DropdownMenuItem className="cursor-pointer">
-			<Link
-				href="/api/auth/logout"
-				className="flex w-full items-center gap-2 text-destructive"
-			>
-				<LogOut className="h-4 w-4" /> Logout
-			</Link>
+			<form action={logout}>
+				<button className="flex w-full items-center gap-2 text-destructive">
+					<LogOut className="h-4 w-4" /> Logout
+				</button>
+			</form>
 		</DropdownMenuItem>
 	);
 };
