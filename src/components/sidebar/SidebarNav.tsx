89f8ae5f584a1a124f271b80f23ea03eb5222979
Stanisław Synowiec <stasyn1410@gmail.com>
2024-08-21 13:58:12 +0200

    chore(sidebar): sidebar nav

---
+++ src/components/sidebar/SidebarNav.tsx
@@ -0,0 +1,56 @@
+import { LayoutDashboard, Settings } from 'lucide-react';
+import { SidebarNavIcon } from '@/components/sidebar/SidebarNavIcon';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { SidebarNavLink } from '@/components/sidebar/SidebarNavLink';
+import type { ReactNode } from 'react';
+
+const NAV_LINKS = [
+	{
+		href: '/dashboard',
+		icon: <LayoutDashboard />,
+		children: 'Dashboard',
+	},
+	{
+		href: '/settings',
+		icon: <Settings />,
+		children: 'Settings',
+	},
+];
+
+interface SidebarNavProps {
+	isCollapsed: boolean;
+}
+
+export const SidebarNav = ({ isCollapsed }: SidebarNavProps) => {
+	const renderSidebarNavLinkAndIcon = (
+		href: string,
+		icon: string | ReactNode,
+		children: string,
+	) => {
+		if (isCollapsed) {
+			return (
+				<SidebarNavIcon href={href} icon={icon} key={href}>
+					{children}
+				</SidebarNavIcon>
+			);
+		}
+
+		return (
+			<SidebarNavLink href={href} icon={icon} key={href}>
+				{children}
+			</SidebarNavLink>
+		);
+	};
+
+	return (
+		<TooltipProvider delayDuration={0}>
+			<nav className="flex-1">
+				<ul className="flex flex-col gap-1">
+					{NAV_LINKS.map(({ href, icon, children }) =>
+						renderSidebarNavLinkAndIcon(href, icon, children),
+					)}
+				</ul>
+			</nav>
+		</TooltipProvider>
+	);
+};
