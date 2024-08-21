5cb60ea71b873449f1a5ef49accb77bca71aa540
Stanisław Synowiec <stasyn1410@gmail.com>
2024-08-21 14:17:56 +0200

    fix(auth): logout button size

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -5,7 +5,7 @@ import { LogOut } from 'lucide-react';
 export const LogoutButton = () => {
 	return (
 		<DropdownMenuItem className="cursor-pointer">
-			<form action={logout}>
+			<form action={logout} className="w-full">
 				<button className="flex w-full items-center gap-2 text-destructive">
 					<LogOut className="h-4 w-4" /> Logout
 				</button>
