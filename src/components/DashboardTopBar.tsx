3e5a037a72295bd2ae1cf1170f3f3dbda023b128
Stanisław Synowiec <stasyn1410@gmail.com>
2024-08-21 15:37:18 +0200

    chore(dashboard): add top bar

---
+++ src/components/DashboardTopBar.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { Moon, Sun } from 'lucide-react';
+import { useTheme } from 'next-themes';
+
+export const DashboardTopBar = () => {
+	const { theme, setTheme } = useTheme();
+
+	const changeTheme = () => {
+		setTheme(theme === 'dark' ? 'light' : 'dark');
+	};
+
+	return (
+		<div className="flex items-center justify-between border-b-2 border-b-muted px-6 py-3">
+			<div className="gap -4 flex"></div>
+			<Button variant="outline" size="icon" onClick={changeTheme}>
+				{theme === 'dark' ? <Sun /> : <Moon />}
+				<span className="sr-only">
+					Change theme to {theme === 'dark' ? 'light' : 'dark'}
+				</span>
+			</Button>
+		</div>
+	);
+};
--- src/components/sidebar/SidebarHeader.tsx
+++ src/components/sidebar/SidebarHeader.tsx
@@ -7,8 +7,8 @@ interface SidebarHeaderProps {
 
 export const SidebarHeader = ({ isCollapsed }: SidebarHeaderProps) => {
 	return isCollapsed ? (
-		<div className="flex h-12 w-full items-center justify-center border-b-2 border-b-muted px-6 py-4 md:p-0">
-			<div className="gap -4 flex items-center">
+		<div className="flex h-[4.1rem] w-full items-center justify-center border-b-2 border-b-muted px-6 py-4 md:p-0">
+			<div className="gap -4 m-2 flex items-center">
 				<Link
 					href="/"
 					className="flex w-full items-center justify-center gap-2"
