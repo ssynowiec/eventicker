4e02c585a1dd15e63af531cda30be5a238bd239a
Stanisław Synowiec <stasyn1410@gmail.com>
2024-08-22 19:10:19 +0200

    style(theme): add light/dark variant logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,17 @@
+'use client';
+
 import Image from 'next/image';
+import { useTheme } from 'next-themes';
 
 export const Logo = () => {
+	const { theme } = useTheme();
+
+	const logoColor = theme === 'dark' ? 'light' : 'dark';
+	console.log(theme);
+
 	return (
 		<Image
-			src="/draft-logo.png"
+			src={`/draft-logo-${logoColor}.png`}
 			alt="company logo"
 			width={100}
 			height={100}
