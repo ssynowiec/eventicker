a5c67680041a075724ac37e8ddeecddff099d2d3
Stanisław Synowiec <stasyn1410@gmail.com>
2024-08-22 14:17:38 +0200

    chore(i18n): add not found translations

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,18 +1,18 @@
 import { buttonVariants } from '@/components/ui/button';
 import Link from 'next/link';
 import { GoBackButton } from '@/components/GoBackButton';
+import { getTranslations } from 'next-intl/server';
 
-const NotFoundPage = () => {
+const NotFoundPage = async () => {
+	const t = await getTranslations('NotFound');
+
 	return (
 		<main className="h-svh">
-			<div className="m-auto flex h-full w-full flex-col items-center justify-center gap-2">
+			<div className="m-auto flex h-full w-full flex-col items-center justify-center gap-4">
 				<h1 className="text-9xl font-bold">404</h1>
-				<span className="font-medium">Oops! Page Not Found!</span>
-				<p className="text-center text-muted-foreground">
-					It seems like the page you&apos;re looking for <br />
-					does not exist or might have been removed.
-				</p>
-				<div className="mt-6 flex gap-4">
+				<span className="font-medium">{t('title')}</span>
+				<p className="text-center text-muted-foreground">{t('message')}</p>
+				<div className="flex gap-4">
 					<GoBackButton />
 					<Link href="/" className={buttonVariants()}>
 						Back to Home
