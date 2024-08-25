2e1a4ff2f5435309b3cc70bc9d6afdcebf42e54c
Stanisław Synowiec <stasyn1410@gmail.com>
2024-08-25 11:19:35 +0200

    chore(event api): add post route to create event

--- src/app/api/event/route.ts
+++ src/app/api/event/route.ts
@@ -1,8 +1,9 @@
 import { db } from '@/lib/auth/db';
-import { eventTable } from '@/schema/event';
+import { eventTable, insertEventSchema } from '@/schema/event';
 import { NextRequest } from 'next/server';
 import { lucia } from '@/lib/auth/auth';
 import { eq } from 'drizzle-orm';
+import { ZodError } from 'zod';
 
 export const GET = async (req: NextRequest) => {
 	const { user } = await lucia.validateSession(
@@ -25,4 +26,36 @@ export const GET = async (req: NextRequest) => {
 			'Content-Type': 'application/json',
 		},
 	});
+};
+
+export const POST = async (req: NextRequest) => {
+	const { user } = await lucia.validateSession(
+		req.cookies.get('auth_session')?.value ?? '',
+	);
+
+	if (!user) {
+		return new Response('Unauthorized', {
+			status: 401,
+		});
+	}
+
+	try {
+		const newEvent = insertEventSchema.parse(await req.json());
+
+		const event = await db
+			.insert(eventTable)
+			.values({ ...newEvent, creator_id: user.id });
+
+		return new Response(JSON.stringify(event), {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+	} catch (e) {
+		if (e instanceof ZodError) {
+			return new Response(e.message, {
+				status: 400,
+			});
+		}
+	}
 };
