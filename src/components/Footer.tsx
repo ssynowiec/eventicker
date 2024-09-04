e78af5391f0f125181ee0f5e91324580874cbe13
Stanisław Synowiec <stasyn1410@gmail.com>
2024-09-04 11:54:23 +0200

    style(footer): fix dark mode text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const currentYear = new Date().getFullYear();
 
 export const Footer = () => {
 	return (
-		<footer className="bg-foreground p-4 text-center text-sm text-white">
+		<footer className="bg-foreground p-4 text-center text-sm text-white dark:text-black">
 			<p>&copy; {currentYear} Eventicker. All rights reserved.</p>
 		</footer>
 	);
