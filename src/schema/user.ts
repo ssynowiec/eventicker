3c1450fe3fa6076399e0af05fb4f86369713ad5b
Stanisław Synowiec <stasyn1410@gmail.com>
2024-09-10 13:06:36 +0200

    chore(stripe): add stripe field in user table

--- src/schema/user.ts
+++ src/schema/user.ts
@@ -4,6 +4,7 @@ export const userTable = pgTable('user', {
 	id: text('id').primaryKey(),
 	name: text('name').notNull(),
 	username: text('username').notNull(),
+	stripe_account_id: text('stripe_account'),
 	email: text('email').notNull(),
 	avatar: text('avatar'),
 	github_id: text('github_id'),
