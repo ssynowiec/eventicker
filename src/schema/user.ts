cdace78a8ebf2663625004ab058f39051d9b8748
Stanisław Synowiec <stasyn1410@gmail.com>
2024-09-11 20:46:54 +0200

    chore(stripe): add connection status to db

--- src/schema/user.ts
+++ src/schema/user.ts
@@ -1,10 +1,11 @@
-import { pgTable, text } from 'drizzle-orm/pg-core';
+import { boolean, pgTable, text } from 'drizzle-orm/pg-core';
 
 export const userTable = pgTable('user', {
 	id: text('id').primaryKey(),
 	name: text('name').notNull(),
 	username: text('username').notNull(),
 	stripe_account_id: text('stripe_account'),
+	stripeConnected: boolean('stripe_connected').default(false),
 	email: text('email').notNull(),
 	avatar: text('avatar'),
 	github_id: text('github_id'),
